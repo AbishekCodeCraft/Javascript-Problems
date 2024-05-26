dda271716354a4fa13b8e9673f277c568b687e65
Abishek <abishek19022003@gmail.com>
2024-05-26 22:07:09 +0530

    Separated

---
+++ Basic Star Pattern/script.js
@@ -0,0 +1,15 @@
+//Horizontal Line of stars
+var n = 5;
+var res = "";
+for (i = 0; i < n; i++) {
+  res = res + "*";
+}
+console.log(res);
+
+//Vertical Line of stars
+var n = 5;
+var res = "";
+for (i = 0; i < n; i++) {
+  res = res + "*\n";
+}
+console.log(res);
